965869fb80a64933b91d80562752f40344508d43
Lntck <Krutov315@mail.ru>
2025-07-03 22:59:02 +0300

    #80

--- algorithms/python/removeDuplicatesfromSortedArrayII.py
+++ algorithms/python/removeDuplicatesfromSortedArrayII.py
@@ -2,10 +2,17 @@ class Solution:
     # Key idea is to sort Not to remove
     def removeDuplicates(self, nums: List[int]) -> int:
         k = 1
+        double = False
         prev = nums[0]
         for i in range(1, len(nums)):
             if nums[i] != prev:
                 nums[k] = nums[i]
                 prev = nums[i]
                 k += 1
+                double = False
+            else:
+                if not double:
+                    nums[k] = nums[i]
+                    k += 1
+                double = True
         return k
