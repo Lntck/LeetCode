53dc6f12582c33d5523438b7b43d781259554af4
Lntck <Krutov315@mail.ru>
2025-09-25 21:36:58 +0300

    #82

---
+++ algorithms/python/removeDuplicatesFromSortedListII.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        dummy = ListNode(-1, head)
+
+        prev = dummy
+        curr = head
+        while curr and curr.next:
+            if curr.val == curr.next.val:
+                while curr.next and curr.val == curr.next.val:
+                    curr = curr.next
+                prev.next = curr.next
+            else:
+                prev = prev.next
+            curr = curr.next
+        return dummy.next
