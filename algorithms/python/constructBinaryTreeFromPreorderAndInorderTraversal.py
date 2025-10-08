7b6293d5d65e4054e95fa10a830a8081f5ac815d
Lntck <Krutov315@mail.ru>
2025-10-08 13:45:04 +0300

    #105

--- algorithms/python/constructBinaryTreeFromPreorderAndInorderTraversal.py
+++ algorithms/python/constructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -7,4 +7,17 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, preorder: list[int], inorder: list[int]) -> TreeNode | None:
-        pass
+        val_to_index = {val: index for index, val in enumerate(inorder)}
+
+        def build(pre_start, pre_end, in_start, in_end):
+            if pre_start > pre_end or in_start > in_end: return None
+            root = TreeNode(preorder[pre_start])
+            root_index = val_to_index[root.val]
+
+            left_size = root_index - in_start
+
+            root.left = build(pre_start + 1, pre_start + left_size + 1, in_start, root_index - 1)
+            root.right = build(pre_start + 1 + left_size, pre_end, root_index + 1, in_end)
+            return root
+
+        return build(0, len(preorder) - 1, 0, len(inorder) - 1)
