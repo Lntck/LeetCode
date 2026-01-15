709df6f3ad3bc3405b56f5898128d245e0ecec25
Rushan <Krutov315@mail.ru>
2026-01-15 19:48:48 +0300

    #148

---
+++ algorithms/python/sortList.py
@@ -0,0 +1,62 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def sortList(self, head: ListNode | None) -> ListNode | None:
+        def split(head: ListNode | None, size: int) -> tuple[ListNode | None, ListNode | None]:
+            current = head
+            for _ in range(size - 1):
+                if not current: break
+                current = current.next
+            
+            if not current: return head, None
+
+            next_part = current.next
+            current.next = None
+            return head, next_part
+
+
+        def merge(left: ListNode | None, right: ListNode | None) -> tuple[ListNode | None, ListNode | None]:
+            dummy = ListNode(0)
+            cur = dummy
+            while left and right:
+                if left.val < right.val:
+                    cur.next = left
+                    left = left.next
+                else:
+                    cur.next = right
+                    right = right.next
+                cur = cur.next
+            cur.next = left if left else right
+
+            while cur.next:
+                cur = cur.next
+
+            return dummy.next, cur
+
+        n = 0
+        cur = head
+        while cur:
+           n += 1
+           cur = cur.next
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        size = 1
+        while size < n:
+            previous = dummy
+            current = dummy.next
+
+            while current:
+                part1, next_part = split(current, size)
+                part2, current = split(next_part, size)
+
+                merged_head, merged_tail = merge(part1, part2)
+                previous.next = merged_head
+                previous = merged_tail
+            size *= 2
+        return dummy.next
