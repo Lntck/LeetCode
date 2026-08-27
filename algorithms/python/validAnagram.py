4328f9e9897c4e55617a0cb43daa37824c13b283
Rushan <Krutov315@mail.ru>
2026-08-27 15:50:05 +0300

    #242

--- algorithms/python/validAnagram.py
+++ algorithms/python/validAnagram.py
@@ -1,3 +1,15 @@
 class Solution:
+    # Time complexity: O(n), Space complexity: O(n)
     def isAnagram(self, s: str, t: str) -> bool:
-        return {ch: s.count(ch) for ch in set(s)} == {ch: t.count(ch) for ch in set(t)}
+        if len(s) != len(t):
+            return False
+        
+        sl1 = {}
+        for ch in s:
+            sl1[ch] = sl1.get(ch, 0) + 1
+        
+        for ch in t:
+            if sl1.get(ch, 0) == 0:
+                return False
+            sl1[ch] -= 1
+        return True
